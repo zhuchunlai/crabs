f31124793bf225ecc24893aebfbed99b5158c45f
zhuchunlai <zhuchunlai206@gmail.com>
2014-09-04 17:58:20 +0800

    rename

--- crabs-common/src/main/java/com/code/crabs/exception/SQL4ESException.java
+++ crabs-common/src/main/java/com/code/crabs/exception/SQL4ESException.java
@@ -1,7 +1,24 @@
 package com.code.crabs.exception;
 
 /**
- * Created by zhuchunlai on 14-9-4.
+ * General exception for crabs.
+ *
+ * @author zhuchunlai
+ * @version $Id: SQL4ESException.java, v1.0 2014/08/01 17:53$
  */
-public class SQL4ESException {
+public class SQL4ESException extends Exception {
+
+    public SQL4ESException(final String message){
+        super(message);
+    }
+
+    public SQL4ESException(final Exception e){
+        super(e.getMessage(), e);
+    }
+
+    public SQL4ESException(final String message, final Throwable cause){
+        super(message, cause);
+    }
+
+
 }
